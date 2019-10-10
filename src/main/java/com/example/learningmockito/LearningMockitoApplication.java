5656258ce941af61b7862bff980aefd782ca2453
jhorlyn jaime santos rosales <j.santos@costamar.com>
2019-10-10 15:44:51 -0500

    se modifico el LearningMockitoApplication

--- src/main/java/com/example/learningmockito/LearningMockitoApplication.java
+++ src/main/java/com/example/learningmockito/LearningMockitoApplication.java
@@ -2,6 +2,7 @@ package com.example.learningmockito;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class LearningMockitoApplication {
@@ -9,5 +10,5 @@ public class LearningMockitoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LearningMockitoApplication.class, args);
 	}
-
+		System.out.println("Inicializando");
 }
